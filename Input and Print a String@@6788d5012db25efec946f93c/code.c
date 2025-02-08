7403659c585c47647115f31985729d3b70686cfd
AnkitaSharma262 <ankita0262.becse24@chitkara.edu.in>
2025-02-08 12:59:58 +0530

    Solution to Input and Print a String

---
+++ Input and Print a String@@6788d5012db25efec946f93c/code.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// char* welcome() {
+//     return "Welcome to Code Radar!";
+// }
+
+  int main() {
+    char string;
+    printf("Enter a single word:");
+    scanf(" %c",&string);
+    printf("You entered :%c\n",string);
+    return 0;
+}
